b3c1c1a861344342cfa14adfdb51f550d460d0cb
AHMED AFANDY <eng.computer1@yahoo.com>
2020-06-30 21:16:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,14 @@ class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
 get  '/' do
+<<<<<<< HEAD
 
   "Hello, World!"
+=======
+  "Ahmed Afandy !!!"
+end
+
+>>>>>>> fbd9d28b03bac98d06d87dda6e0b781dd0436f41
 end
 
 end
